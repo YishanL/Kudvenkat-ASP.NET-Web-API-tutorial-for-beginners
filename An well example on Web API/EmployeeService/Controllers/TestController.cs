e1b343b1ddbf3b539719d4af98cd0e824f69cba5
YishanL <liyishan1108@gmail.com>
2018-11-22 22:28:28 +1300

    ASP .NET Web API user register, login, load employees, log out with bearer token based authentication.

---
+++ An well example on Web API/EmployeeService/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace EmployeeService.Controllers
+{
+    public class TestController : ApiController
+    {
+        public string Get()
+        {
+            return "Hello from TestController";
+        }
+    }
+}
